6f8015747c9d9cae863811f51484d7b8918a9eb3
Chris Rodgers <cj.rodgers@gmail.com>
2022-10-06 14:54:30 +0100

    Next step: Navigation

---
+++ src/app/services/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heros';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  getHeroes(): Observable<Hero[] >{
+    const heroes = of(HEROES)
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+
+  constructor(private messageService: MessageService) {}
+}
